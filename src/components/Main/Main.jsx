50f80b4c3b76f643500efe33a57225229fda9a3b
Muhammed Salman <salman8official@gmail.com>
2024-11-04 10:02:38 +0530

    04Nov1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -60,7 +60,7 @@ const Main = () => {
           }
 
           
-          
+            
           <div className="main-bottom">
             <div className="search-box">
               <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,6 +12,9 @@ const ContextProvider = (props)=>{
     const [loading,setLoading] = useState(false);
     const [resultData,setResultData] = useState("");
 
+    const delayPara=(index,nextWord)=>{
+
+    }
     const onSent = async(prompt)=>{
 
         setResultData("")
@@ -19,7 +22,18 @@ const ContextProvider = (props)=>{
         setShowResult(true)
         setRecentPrompt(input)
         const response = await run(input)
-        setResultData(response)
+        let responseArray = response.split("**")
+        let newResponse;
+        for(let i=0;i<responseArray.length;i++){
+            if(i==0 || i%2==0){
+                newResponse += responseArray[i]
+            }
+            else{
+                newResponse += "<b>" + responseArray[i]+"</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>");
+        setResultData(newResponse2)
         setLoading(false)
         setInput("")
 
